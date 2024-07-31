49a5f8896f0a8ab9a306bd15a433727193e2576a
Chathurni Ratwatte <85515751+cssratwatte@users.noreply.github.com>
2024-08-01 03:31:03 +1000

    Update VectorAdd.cpp

--- Module 2/M2.S2P/VectorAdd.cpp
+++ Module 2/M2.S2P/VectorAdd.cpp
@@ -2,59 +2,59 @@
 #include <cstdlib>
 #include <time.h>
 #include <chrono>
+#include <thread>
+
 
 using namespace std::chrono;
 using namespace std;
 
-// Function to fill a vector with random values
 void randomVector(int vector[], int size)
 {
     for (int i = 0; i < size; i++)
     {
-        // Generate a random number between 0 and 99
+        // get random number modulo 100 and set at location i in array
         vector[i] = rand() % 100;
     }
 }
 
+
 int main(){
+
     unsigned long size = 100000000;
 
     srand(time(0));
 
     int *v1, *v2, *v3;
 
-    // Record the start time of the entire operation
+    // get the current time using this c++ implementation's clock with the smallest tick period / finest resolution
     auto start = high_resolution_clock::now();
 
-    // Allocate memory for the vectors
-    v1 = (int *) malloc(size * sizeof(int));
-    v2 = (int *) malloc(size * sizeof(int));
-    v3 = (int *) malloc(size * sizeof(int));
+    // for each of v1, v2, v3       
+    // allocate contiguous space in heap of size * size in bytes of pointer to int
+    // cast as usable pointer
+    v1 = (int *) malloc(size * sizeof(int *));
+    v2 = (int *) malloc(size * sizeof(int *));
+    v3 = (int *) malloc(size * sizeof(int *));
 
-    // Fill vectors v1 and v2 with random values
+
     randomVector(v1, size);
+
     randomVector(v2, size);
 
-    // Add the corresponding elements of v1 and v2, store the result in v3
+
+    // for loop counting up from 0 to size variable. element at v3[i] = sum of v1[i] and v2[i]
     for (int i = 0; i < size; i++)
     {
         v3[i] = v1[i] + v2[i];
     }
 
-    // Record the end time of the entire operation
     auto stop = high_resolution_clock::now();
 
-    // Calculate the duration of the operation
+    // time taken to run = stop time minus start time, then cast to microseconds
     auto duration = duration_cast<microseconds>(stop - start);
 
-    // Print the time taken by the function
-    cout << "Time taken by function: "
-         << duration.count() << " microseconds" << endl;
 
-    // Free allocated memory
-    free(v1);
-    free(v2);
-    free(v3);
+    cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
 
     return 0;
 }
